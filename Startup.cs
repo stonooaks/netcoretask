46df539834a99eb997cd9d69e0945f1af3101c60
mah264 <hughesm@musc.edu>
2016-06-08 08:33:31 -0400

    Initial Commit

---
+++ Startup.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace netcoretask
+{
+    public class Startup {
+
+        public Startup(IHostingEnvironment env){
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        
+        public void ConfigureServices(IServiceCollection services){
+            services.AddMvc();
+        }
+
+        private IConfigurationRoot Configuration;
+        public void Configure(IHostingEnvironment env, IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+ 
+            app.UseStatusCodePages();
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
+            app.Run(async context => {
+                await context.Response.WriteAsync("There is a problem with MVC");
+            });
+        }
+
+      
+    }
+}
